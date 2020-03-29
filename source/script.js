d19825dc31c5b956a5bc96637928a4b7a09bef50
Chuck King <cking@surveycto.com>
2020-03-29 15:24:39 -0400

    Merge in updates from baseline-text

--- source/script.js
+++ source/script.js
@@ -139,3 +139,14 @@ function clearAnswer() {
     timePassed = 0;
     counter = 0;
 }
+
+// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < and > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
+function unEntity(str){
+    return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+}
+if (fieldProperties.LABEL) {
+    document.querySelector(".label").innerHTML = unEntity(fieldProperties.LABEL);
+}
+if (fieldProperties.HINT) {
+    document.querySelector(".hint").innerHTML = unEntity(fieldProperties.HINT);
+}
